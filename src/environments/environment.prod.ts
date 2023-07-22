dd674a93f8fc604cd88d3b0eb8cd6caeed92fd52
Imola Ádám <adam.imola@marktsoft.hu>
2023-07-22 12:58:45 +0200

    #1 Imported Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'nerve-4d316',
+    appId: '1:492011747353:web:3a4f555f26db02a2f95288',
+    storageBucket: 'nerve-4d316.appspot.com',
+    apiKey: 'AIzaSyApRI-jL1hdYRCUJj8WT-ixx--0BfWUpD4',
+    authDomain: 'nerve-4d316.firebaseapp.com',
+    messagingSenderId: '492011747353',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'nerve-4d316',
+    appId: '1:492011747353:web:3a4f555f26db02a2f95288',
+    storageBucket: 'nerve-4d316.appspot.com',
+    apiKey: 'AIzaSyApRI-jL1hdYRCUJj8WT-ixx--0BfWUpD4',
+    authDomain: 'nerve-4d316.firebaseapp.com',
+    messagingSenderId: '492011747353',
+  },
   production: false
 };
 
